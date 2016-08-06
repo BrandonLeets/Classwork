7627bacd78636b050e08ccd136426d4c6d5f766f
Brandon <BigRigLeets@Gmail.com>
2016-08-06 14:27:30 -0700

    Really Final Commit

--- ProgrammingAssignment2-master/cachematrix.R
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -5,46 +5,31 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 ## make the special matrix similar to amking a special vector
-	m <- NULL
-	set <- function(y){
+	mat <- NULL
+	setMatrix <- function(y){
 		x <<- y
-		m <<- NULL
+		mat <<- NULL
 	}
-	get <- function() x
-	setinverse <- function(inverse) m <<- inverse
-	getinverse <- function() m
-	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+	getmatrix <- function() x
+	setinverse <- function(inverse) mat <<- inverse
+	getinverse <- function() mat
+	list(setMatrix = setMatrix, getmatrix = getmatrix, setinverse = setinverse, getinverse = getinverse)
 }
 
 ## Write a short comment describing this function
 ## This function takes a matrix and solves the inverse using the determinant function programmed in R
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, .	q.) {
   ##Take the inverse of the matrix
   ## check if there is already a inverse
   m <- x$getinverse()
-  if(!is.null(m)){
+  if(!is.null(mat)){
   	message("getting cached data")
-  	return(m)
+  	return(mat)
   }
   ##else we calcualte the mean
-  else{
-  ##Assign matrix elemetns to  dummy variables
-  a <- x[1,1]
-  b <- x[1,2]
-  c <- x[2,1]
-  d <- x[2,2]
-
-  ##solve the determinet of the matrix
-  det <- det(x)
-  
-  ##reassign the elements of the matrix
-  newMatrix <- matrix(c(d,-b,-c, a),2,2)
-  
-  ##multiply newMatrix by inverse of det
-  inverseMatrix <- (1/det)*newMatrix
-  
-  ##Return inversematrix
-  return(inverseMatrix)
-	}
+  newData <- x%getmatrix()
+  m <- solve(newData,...)
+  x%setMatrix(m)
+  m
   ## Return a matrix that is the inverse of 'x'
 }
